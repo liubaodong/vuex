524388c19e4c535a85372e105bb642b9006349cc
sir-lbd <2458825436@qq.com>
2019-12-11 20:35:27 +0800

    初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
+<!--
+ * @description: 头部描述
+ * @Author: Coder
+ * @Date: 2019-12-11 20:21:40
+ * @LastEditors: Coder
+ * @LastEditTime: 2019-12-11 20:34:35
+ * @FilePath: \vuex\src\views\Home.vue
+ -->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
